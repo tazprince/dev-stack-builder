aeaffa91e8b8971f3bb7a3d2d8a960c27fbc1a1c
tazprince <tazbirkhanprince@gmail.com>
2026-09-12 20:55:19 +0600

    banner section added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Banner from './components/Banner/Banner'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
+      <Banner></Banner>
     </>
   )
 }
